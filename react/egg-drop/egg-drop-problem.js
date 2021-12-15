3a474f8dfee3a79ac4da11d0c3b66af6ddf94013
Jason Buchanan <jsnbuchanan@gmail.com>
2021-12-15 09:36:01 -0700

    feat: added egg-drop-problem.js for Egg Drop in Two
    - added test setup with tests for failure and success
    - even after success the test will coach you to encourage
      a more optimal algorithm attempt
    
    The test will intentionally fail until getLowestBreakingFloor()
    is implemented. The console output will look something like this:
    
     EggCache { _eggs: [ Egg { _broken: false }, Egg { _broken: false } ] }
    
    CHEAT SHEET:
      .-----------------------------.
      | floor 5     ೃ*.˚broken˚.*ೃ   |
      |-----------------------------|
      | floor 4                      |
      |-----------------------------|
      | floor 3                      |
      |-----------------------------|
      | floor 2                      |
      |-----------------------------|
      | floor 1                      |
      |                             |
      |    []  []   .-.   []  []    |
      |    []  []   |||   []  []    |
    --'-------------'''-------------'--
    ===================================
       Egg will break at floor 5!
    
    Test Failed!
    expected: 5
    actual: -1

---
+++ react/egg-drop/egg-drop-problem.js
@@ -0,0 +1,69 @@
+const EggCache = require("./EggCache");
+const Building = require("./Building");
+
+/**
+ * Credit for this test goes to David Cross and Scott Hawker (https://twitter.com/yarell)
+ * at CommerceHub.
+ *
+ * We're going to do an experiment using a 100-story building and 2 eggs.
+ *
+ * Using as few drops as possible, you'll need to drop the eggs from
+ * different floors in order to determine the highest floor from which an egg
+ * can be dropped without breaking.
+ *
+ * The following constraints should be observed:
+ *   - You cannot drop an egg from more than 1 floor at a time.
+ *   - You may reuse any eggs that remain unbroken after dropping them.
+ *   - The eggs are the same - if one breaks from the 30th floor, the other will as well.
+ *   - If an egg breaks from a given floor, it will break from all floors above it.
+ *   - If an egg does not break from a given floor, it will not break from all floors below it.
+ *
+ * <strong>Describe the process by which you would make this determination.</strong>
+ *
+ * Keep in mind the goal of this question isn't for you to come up with the answer we’re looking for on your own.
+ *
+ * We hope to gain insight to how you approach & solve a tough problem if we brought you on the team.
+ *
+ * Please feel free to work through ideas & process <strong>with us as your teammates</strong>.
+ *
+ * @param eggs cache with two unbroken eggs
+ * @param building with 100 floors
+ * @returns {number} the lowest floor number where an egg dropped, would break
+ */
+const getLowestBreakingFloor = (eggs, building) => {
+  // TODO Implement an algorithm with the least number of drop attempts
+  console.log(`\n`, eggs);
+  return -1;
+};
+
+(function() {
+  const building = Building.with(5);
+  const eggs = new EggCache();
+
+  const correctAnswer = building.cheat();
+  const answer = getLowestBreakingFloor(eggs, building);
+
+  console.log(building.cheatSheet());
+
+  if (correctAnswer === answer) {
+    console.log(`Test passes${successBot(building.getAttemptCount())}`);
+    console.log(`The egg breaks first on floor number ${answer}.`);
+  } else {
+    console.log('Test Failed!');
+    console.log('expected:', correctAnswer);
+    console.log('actual:', answer);
+  }
+})();
+
+const successBot = (attempts) => {
+  if (attempts > 49) {
+    return `, but you dropped ${attempts} times or more, how might you improve this algorithm? `;
+  } else if (attempts > 29) {
+    return ` and you're warming up. Only ${attempts} attempts that time. Is there a way to cut that attempt count in half?`;
+  } else if (attempts > 20) {
+    return ` and you're getting closer. Only ${attempts} attempts that time. Does anyone on the team have ideas to help?`;
+  } else if (attempts > 14) {
+    return ` and you are getting pretty efficient. Only ${attempts} attempts. Is there anything else you can do?`;
+  }
+  return `. Good work. the Team really nailed it! And only ${attempts} attempts.`;
+}
