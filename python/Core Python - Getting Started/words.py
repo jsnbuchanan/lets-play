73723a51c0baabb3df8888ae025befde5119ed36
Jason Buchanan <jsnbuchanan@gmail.com>
2021-12-30 16:14:42 -0700

    feat: refactoring words module to make it easier to test from the Repl using def main()
    from Core Python: Getting Started by Robert Smallshire, and Austin Bingham

--- python/Core Python - Getting Started/words.py
+++ python/Core Python - Getting Started/words.py
@@ -9,10 +9,18 @@ def fetch_words():
         for word in line_words:
             story_words.append(word)
     story.close()
+    return story_words
 
+
+def print_words(story_words):
     for word in story_words:
         print(word)
 
 
+def main():
+    words = fetch_words()
+    print_words(words)
+
+
 if __name__ == '__main__':
-    fetch_words()
+    main()
